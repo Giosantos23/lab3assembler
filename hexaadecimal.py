9ffdf7925aae2f79ffc23ee21d09a4ff9c77c6b3
Giovanni Santos <san22523@uvg.edu.gt>
2023-02-23 10:21:12 -0600

    Se agrega funcion de convertir decimal a hexadecimal

--- hexaadecimal.py
+++ hexaadecimal.py
@@ -4,7 +4,6 @@ def menu():
     print('\t2 - hola, bienvenido a la conversión de hexadecimal a decimal')
     seleccion = input('Ingrese opcion 1 si desea convertir de decimal a hexadecimal o la opcion 2 si es viceversa')
     if seleccion == '1':
-        print('o')
         decia_exa()
     elif seleccion == '2':
         exaa_deci()
@@ -12,10 +11,13 @@ def menu():
         print('Seleccione 1 o 2')
     menu()
 def decia_exa():
-    print('hey')
+    deci=int(input('Ingrese el numero decimal (sin espacios) que desea convertir: '))
+    hexi=hex(deci)
+    print('El decimal convertido es: ',hexi)
+
 def exaa_deci():
-    hexa = input('Ingrese el numero hexadecimal (en mayusculas) que desea convertir: ').strip().upper()
+    hexa = input('Ingrese el hexadecimal (en mayusculas) que desea convertir: ').strip().upper()
     decimal = int(hexa, base=16)
     #Se utiliza funcion de base 16, la que los hexadecimales tienen
-    print('El decimal convertido es:', decimal)
+    print('El hexadecimal convertido es:', decimal)
 menu()
